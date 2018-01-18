e35ab620ed799f296c9ea3e7fad3ba21a73e66f0
Edgar <edgar.sdsantos@gmail.com>
2018-01-18 12:37:29 -0200

    Dominator problem solved

---
+++ Codibility/Codibility/Problems/Leader/Dominator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codibility.Problems.Leader
+{
+    class Dominator
+    {
+        public static int solution(int[] A)
+        {
+            int N = A.Length;
+            int size = 0;
+            int value = -1;
+
+            for(int i = 0; i < N; i++)
+            {
+                if(size == 0)
+                {
+                    size++;
+                    value = A[i];
+                }
+                else
+                {
+                    if (value != A[i])
+                        size--;
+                    else
+                        size++;
+                }
+            }
+
+            int candidate = -1;
+            if (size > 0)
+                candidate = value;
+            int count = 0;
+            int indexLeader = -1;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] == candidate)
+                {
+                    indexLeader = i;
+                    count++;
+                }
+            }
+
+            if (count > N / 2) {
+                return indexLeader;
+            }
+
+            return -1;
+        }
+    }
+}
--- Codibility/Codibility/Program.cs
+++ Codibility/Codibility/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Codibility.Problems.CountingElements;
+using Codibility.Problems.Leader;
 using Codibility.Problems.PrimeNumbers;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,9 @@ namespace Codibility
         {
             //Debug.WriteLine(PermCheck.solution(new int[] { 4, 1, 3, 2 }));
             //Debug.WriteLine(MissingInteger.solution(new int[] { 1, 3, 6, 4, 1, 2 }));
-            Debug.WriteLine(CountFactors.solution(24));
+            //Debug.WriteLine(CountFactors.solution(24));
+
+            Debug.WriteLine(Dominator.solution(new int[] { 3, 4, 3, 2, 3, -1, 3, 3 }));
         }
     }
 }
