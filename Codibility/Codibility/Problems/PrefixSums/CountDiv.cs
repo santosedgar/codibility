3b91da76eb673bc42a8092c55b2b58a253815398
Edgar <edgar.sdsantos@gmail.com>
2018-01-24 00:34:53 -0200

    CountDiv solution

---
+++ Codibility/Codibility/Problems/PrefixSums/CountDiv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codibility.Problems.PrefixSums
+{
+    public class CountDiv
+    {
+        public static int solution(int A, int B, int K)
+        {
+            return (B / K) - (A / K) + ((A % K == 0) ? 1 : 0);
+        }
+    }
+}
--- Codibility/Codibility/Program.cs
+++ Codibility/Codibility/Program.cs
@@ -2,6 +2,7 @@
 using Codibility.Problems.BinarySearch;
 using Codibility.Problems.CountingElements;
 using Codibility.Problems.Leader;
+using Codibility.Problems.PrefixSums;
 using Codibility.Problems.PrimeNumbers;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,9 @@ namespace Codibility
             //Debug.WriteLine(MissingInteger.solution(new int[] { 1, 3, 6, 4, 1, 2 }));
             //Debug.WriteLine(CountFactors.solution(24));
 
-            Debug.WriteLine(String.Join(",", MaxCounters.solution(5, new int[] { 3,4,4,6,1,4,4 })));
+            //Debug.WriteLine(String.Join(",", MaxCounters.solution(5, new int[] { 3,4,4,6,1,4,4 })));
+
+            Debug.WriteLine(CountDiv.solution(10, 20, 2));
         }
     }
 }
